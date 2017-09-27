467ad4acc948ea704874b407a32304ea6ab54ca4
Thomas Daede <daede003@umn.edu>
2017-09-26 20:09:19 -0700

    Add basic rate limiting.

--- noise_upload_server.js
+++ noise_upload_server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var multer = require('multer');
 var fs = require('fs');
+var RateLimit = require('express-rate-limit');
 
 var app = express();
 
@@ -17,12 +18,20 @@ var storage = multer.diskStorage({
   }
 })
 
+
+var apiLimiter = new RateLimit({
+  windowMs: 60*1000, // 1 minute
+  max: 10, // 10 uploads
+  delayMs: 0 // disabled
+});
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+app.use('/upload',apiLimiter);
 app.use('/upload',multer({dest: './noise_uploads', limits: {files:1, fileSize: 2000000}, storage: storage}).any());
 app.post('/upload', function(req,res) {
   console.log('uploaded',req.file,req.files);
